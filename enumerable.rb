cc6e23144e638fb53b822f654e7c06b7ce476352
jaferIdrees <jaferll@gmail.com>
2022-09-23 01:10:33 +0300

    Add filter method

--- enumerable.rb
+++ enumerable.rb
@@ -1,23 +1,36 @@
+# frozen_string_literal: true
+
 require_relative 'myenumerable'
+
+# Define the MyList class
 class MyList
-    include MyEnumerable
-    def initialize(*list)
-        @list = []
-        list.each { |item| @list << item }
-        @list
+  include MyEnumerable
+
+  def initialize(*param)
+    @list = []
+    param.each { |item| @list << item }
+  end
+
+  def each
+    i = 0
+    while i < @list.size
+      yield @list[i]
+      i += 1
     end
-    def each
-        i = 0
-        while i < @list.size
-            yield @list[i]
-            i += 1
-        end
-    end
+  end
 end
 
+# Create our list
 list = MyList.new(1, 2, 3, 4)
-list.all? {|e| e < 5}
+# puts list.each { |n| puts n }
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
 # Test #any?
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+# Test #filter
+puts(list.filter(&:odd?))
--- myenumerable.rb
+++ myenumerable.rb
@@ -1,13 +1,28 @@
+# frozen_string_literal: true
+
+# Define MyEnumerable module
 module MyEnumerable
   def all?
+    return true unless block_given?
+
     result = true
     each { |item| result &= yield(item) }
     result
   end
 
-def any?
-  result = false
-  each { |item| result |= yield(item) }
-  result
-end
+  def any?
+    return false unless block_given?
+
+    result = false
+    each { |item| result |= yield(item) }
+    result
+  end
+
+  def filter
+    return [] unless block_given?
+
+    result = []
+    each { |item| result << item if yield(item) }
+    result
+  end
 end
