a1d2beec946234fa21b0d6af23c8480ebe30828a
jaferIdrees <jaferll@gmail.com>
2022-09-23 00:27:04 +0300

    Add all method

--- enumerable.rb
+++ enumerable.rb
@@ -1,7 +1,19 @@
+require_relative 'myenumerable'
 class MyList
+    include MyEnumerable
     def initialize(*list)
         @list = []
         list.each { |item| @list << item }
         @list
     end
+    def each
+        i = 0
+        while i < @list.size
+            yield @list[i]
+            i += 1
+        end
+    end
 end
+
+list = MyList.new(1, 2, 3, 4)
+list.all? {|e| e < 5}
--- myenumerable.rb
+++ myenumerable.rb
@@ -1,2 +1,7 @@
 module MyEnumerable
+  def all?
+    result = true
+    each { |item| result &= yield(item) }
+    result
+  end
 end
